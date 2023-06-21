57aa233f5af6afb815e689ef7360dd14b53a1a04
Mieke <mieke.laporte@student.ucll.be>
2023-06-21 15:18:27 +0200

    changes author

--- src/main/java/ucll/gip/gip4_2dezit/controller/AuthorRestController.java
+++ src/main/java/ucll/gip/gip4_2dezit/controller/AuthorRestController.java
@@ -6,8 +6,12 @@ import org.springframework.web.bind.annotation.*;
 import ucll.gip.gip4_2dezit.dtos.AuthorListItemDTO;
 import ucll.gip.gip4_2dezit.model.Author;
 import ucll.gip.gip4_2dezit.dtos.AuthorDTO;
+import ucll.gip.gip4_2dezit.model.Book;
+import ucll.gip.gip4_2dezit.service.exceptions.AuthorBirthDateIsInFutureException;
+import ucll.gip.gip4_2dezit.service.exceptions.AuthorBirthDateIsNullException;
 import ucll.gip.gip4_2dezit.service.exceptions.AuthorNameIsEmptyException;
 import ucll.gip.gip4_2dezit.service.AuthorService;
+import ucll.gip.gip4_2dezit.service.exceptions.AuthorNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
@@ -29,6 +33,32 @@ public class AuthorRestController {
         }
     }
 
+    @PutMapping("/updateAuthor/{id}")
+    public ResponseEntity<Object> updateAuthor(@PathVariable("id") int id, @RequestBody AuthorDTO authorDTO){
+        try{
+            Author author = authorService.updateAuthor(id, authorDTO.toAuthor());
+            AuthorListItemDTO dto = new AuthorListItemDTO();
+            dto.setId(author.getId());
+            dto.setName(author.getName());
+            dto.setBirthDate(author.getBirthDate().toString());
+            dto.setBiography(author.getBiography());
+            for (Book book : author.getBooks()){
+                dto.addBook(book.getIsbnNumber() + ", " + book.getTitle());
+            }
+            return ResponseEntity.ok(dto);
+        } catch (AuthorNotFoundException e){
+            return ResponseEntity.notFound().build();
+        } catch (AuthorNameIsEmptyException e){
+            return ResponseEntity.badRequest().body("name should not be empty");
+        } catch (AuthorBirthDateIsNullException e){
+            return ResponseEntity.badRequest().body("birthdate should not be empty");
+        } catch (AuthorBirthDateIsInFutureException e){
+            return ResponseEntity.badRequest().body("birthdate should not be in the future");
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body("Message: " + e.getMessage());
+        }
+    }
+
     @GetMapping("/getAuthors")
     public ResponseEntity<Object> getAuthors(){
         try {
@@ -40,12 +70,35 @@ public class AuthorRestController {
     }
 
     @GetMapping("/getAuthorById/{id}")
-    public Optional<Author> getAuthorById(@PathVariable("id") int id){
-        return authorService.findAuthorById(id);
+    public ResponseEntity<Object> getAuthorById(@PathVariable("id") int id){
+        try {
+            Optional<Author> author = authorService.findAuthorById(id);
+            AuthorListItemDTO authorListItemDTO = new AuthorListItemDTO();
+            if (author.isPresent()){
+                authorListItemDTO.setId(author.get().getId());
+                authorListItemDTO.setName(author.get().getName());
+                authorListItemDTO.setBirthDate(author.get().getBirthDate().toString());
+                authorListItemDTO.setBiography(author.get().getBiography());
+                for (Book book : author.get().getBooks()){
+                    authorListItemDTO.addBook(book.getIsbnNumber() + ", " + book.getTitle());
+                }
+                return ResponseEntity.ok(authorListItemDTO);
+            }
+            return ResponseEntity.badRequest().body("author is empty");
+        } catch (AuthorNotFoundException e){
+            return ResponseEntity.notFound().build();
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
-    @PostMapping("/deleteAuthorById/{id}")
-    public void deleteAuthorById(@PathVariable("id") int id){
-        authorService.deleteAuthorById(id);
+    @DeleteMapping("/deleteAuthorById/{id}")
+    public ResponseEntity<Object> deleteAuthorById(@PathVariable("id") int id){
+        try {
+            authorService.deleteAuthorById(id);
+            return ResponseEntity.ok("author with id " + id + " is deleted");
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body("Message: " + e.getMessage());
+        }
     }
 }
--- src/main/java/ucll/gip/gip4_2dezit/service/AuthorService.java
+++ src/main/java/ucll/gip/gip4_2dezit/service/AuthorService.java
@@ -9,6 +9,7 @@ import ucll.gip.gip4_2dezit.repository.AuthorRepo;
 import ucll.gip.gip4_2dezit.service.exceptions.AuthorBirthDateIsInFutureException;
 import ucll.gip.gip4_2dezit.service.exceptions.AuthorBirthDateIsNullException;
 import ucll.gip.gip4_2dezit.service.exceptions.AuthorNameIsEmptyException;
+import ucll.gip.gip4_2dezit.service.exceptions.AuthorNotFoundException;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -33,6 +34,23 @@ public class AuthorService {
         return authorRepo.save(author);
     }
 
+    public Author updateAuthor(int id, Author updateAuthor){
+        if (updateAuthor.getName() == null || updateAuthor.getName().isBlank()){
+            throw new AuthorNameIsEmptyException("author name is empty, please provide a name");
+        }
+        if (updateAuthor.getBirthDate() == null){
+            throw new AuthorBirthDateIsNullException();
+        }
+        else if (updateAuthor.getBirthDate().isAfter(LocalDate.now())){
+            throw new AuthorBirthDateIsInFutureException();
+        }
+        Author author = findAuthorById(id).orElseThrow(AuthorNotFoundException::new);
+        author.setName(updateAuthor.getName());
+        author.setBirthDate(updateAuthor.getBirthDate());
+        author.setBiography(updateAuthor.getBiography());
+        return authorRepo.save(author);
+    }
+
     public List<AuthorListItemDTO> getAuthors(){
         List<Author> authorList = authorRepo.findAll();
         List<AuthorListItemDTO> authorListItemDTOS = new ArrayList<AuthorListItemDTO>();
@@ -51,7 +69,11 @@ public class AuthorService {
     }
 
     public Optional<Author> findAuthorById(int id){
-        return authorRepo.findById(id);
+        Optional<Author> author = authorRepo.findById(id);
+        if (author.isEmpty()){
+            throw new AuthorNotFoundException();
+        }
+        return author;
     }
 
     public Optional<Author> findAuthorByName(String name){
